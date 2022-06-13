1b0d4dd8c08edd860c6aea33df91690a853ceaf5
Hoan Bui-Xuan <hoanbx75@gmail.com>
2022-06-13 16:28:44 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+/*
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end('Voilà la réponse du serveur !');
+});
+
+server.listen(process.env.PORT || 3001);
+
+
+*/
+
+
+console.log ("hello world  !!");
+
+
