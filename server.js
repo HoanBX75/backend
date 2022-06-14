b4ae45f2af8b3de5f47ba5031f14bf67c6406b50
Hoan Bui-Xuan <hoanbx75@gmail.com>
2022-06-14 14:49:15 +0200

    commit

--- server.js
+++ server.js
@@ -1,19 +1,73 @@
-
-
-
 const http = require('http');
+const app = require('./app');
 
+const normalizePort = val => {
+  const port = parseInt(val, 10);
 
-console.log ("begin !!");
-app.set('port', process.env.PORT || 3001)
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
 
+const server = http.createServer(app);
 
-console.log ("hello end  !!");
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+server.listen(port);
 
 
 /*
+console.log ("begixn !!");
 
+const http = require('http');
+const app = require ('./app');
+app.set('port', process.env.PORT || 3000);
+const server = http.createServer(app);
+server.listen(process.env.PORT || 3000);
+
+
+console.log ("end !!");
+*/
+
+/*
+
+app.set('port', process.env.PORT || 3001)
+
+const server = http.createServer((req, res) => {
+    console.log ("call back reponse ")
+    res.end('Voilà la réponse df du serveur !');
+});
 const app = require ('./app');
 
 
