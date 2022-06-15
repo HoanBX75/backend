e773ce9a2a1a3cc7cfd799d9716b59550eb34cc8
Hoan Bui-Xuan <hoanbx75@gmail.com>
2022-06-15 16:35:12 +0200

    yes

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,4 +1,22 @@
+const express = require('express');
+const router = express.Router();
 
+const auth = require('../middleware/auth');
+const stuffCtrl = require('../controllers/stuff');
+
+console.log ("routes stuff begin ");
+
+router.get('/', auth, stuffCtrl.getAllStuff);
+router.post('/', auth, stuffCtrl.createThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+router.put('/:id', auth, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
+
+module.exports = router;
+
+
+
+/*
 
 
 const express = require('express');
@@ -14,7 +32,7 @@ router.delete('/:id', stuffCtrl.deleteThing);
 
 module.exports = router;
 
-
+*/
 
 
 
