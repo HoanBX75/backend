7ad7a8320663ff50c6e368e4045eb909258bcb85
Hoan Bui-Xuan <hoanbx75@gmail.com>
2022-06-15 09:44:21 +0200

    commit

--- backup_app.js
+++ backup_app.js
@@ -1,41 +1,5 @@
 
 const express = require('express');
-const bodyParser = require ('body-parser');
-const mongoose = require('mongoose');
-const stuffRoutes = require('./routes/stuff');
-const userRoutes = require('./routes/user');
-
-
-mongoose.connect('mongodb+srv://ochoan:ochoan@cluster0.kpc7n.mongodb.net/?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
-
-
-const app = express();
-
-
-app.use((req, res, next) => {
-    console.log ("here in apps");
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
-  
-app.use(express.json());
-app.use('/api/stuff', stuffRoutes);
-app.use('/api/auth', userRoutes);
-
-module.exports = app;
-
-console.log("apps.js fin");
-
-
-
-/*
-const express = require('express');
 const mongoose = require('mongoose');
 const Thing = require('./models/thing');
 
@@ -105,4 +69,107 @@ app.use((req, res, next) => {
 
 module.exports = app;
   
+
+
+
+/*
+  app.post('/api/stuff', (req, res, next) => {
+
+
+    console.log(JSON.stringify(req.headers));
+
+    console.log(req.body);
+   
+    
+    res.status(201).json({
+      message: 'Objet créé !'
+    });
+  });
+
+ */
+
+
+
+
+  /*
+app.get('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
+  });
+
 */
+
+
+
+
+
+/*
+
+app.use((req, res, next) => {
+  console.log('Requête reçue !');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  console.log ("status = " + res.statusCode);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: 'Votre requête a bien été reçue !' });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Réponse envoyée avec succès !');
+});
+
+module.exports = app;
+*/
+
+
+/*
+console.log ("app begin");
+const express = require('express');
+
+const app = express();
+
+app.use((req, res) => {
+    res.json({ message: 'Votre requête dd dans app a bien été reçue !' }); 
+ });
+ 
+
+module.exports = app;
+
+console.log ("app end");
+
+*/
+
+
+
+/*
+const server = http.createServer((req, res) => {
+    console.log ("cpipic")
+    res.end('Voilà la réponse d du serveur !');
+});
+server.listen(process.env.PORT || 3001);
+*/
+
